9d411f957fbc24fed90bcd6861118d92a06f61c2
Varahmihir <ayushkumar.rv10@gmail.com>
2023-07-04 12:59:29 +0530

    First commit Message

---
+++ employee/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.urls import path
+class Employee(models.Model):  
+    eid = models.CharField(max_length=20)  
+    ename = models.CharField(max_length=100)  
+    eemail = models.EmailField()  
+    econtact = models.CharField(max_length=15)  
+    class Meta:  
+        db_table = "employee"  
+# Create your models here.
